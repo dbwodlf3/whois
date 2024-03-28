d1a79aabb34a70e9256da364bd0750d03af4dee1
Jaeil Yu <jiyu@imagoworks.ai>
2024-03-28 09:35:50 +0900

    update and publish related sanitize

--- test/test.ts
+++ test/test.ts
@@ -1,16 +1,15 @@
 import fs from "fs";
-import path from "path";
-import chai from "chai";
 import { whois, whoisParser } from "index";
+import path from "path";
 
 const domains = JSON.parse(
-    fs.readFileSync(path.resolve(path.join(__dirname,"url-list.json")), "utf-8")
+  fs.readFileSync(path.resolve(path.join(__dirname, "url-list.json")), "utf-8")
 )["domains"];
 
-describe("Function", ()=>{
-    it("whois test", async ()=>{
-        for(const domain of domains) {
-            const result = whoisParser(await whois(domain));
-        }
-    }).timeout(10000);
-})
+describe("Function", () => {
+  it("whois test", async () => {
+    for (const domain of domains) {
+      const result = whoisParser(await whois(domain));
+    }
+  }).timeout(10000);
+});
